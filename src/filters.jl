c9b6e1cd54a03fc92efcc4274c818a8a5ebcfc6c
Oliver Schulz <oschulz@mpp.mpg.de>
2019-03-13 01:34:58 +0100

    STASH

--- src/filters.jl
+++ src/filters.jl
@@ -10,15 +10,9 @@ end
 
 function cr_filter(RC::Real)
     T = float(typeof(RC))
-    α = 1 / (1 + RC)
-    Biquad(T(α), T(-α), T(0), T(α - 1), T(0))
-end
-
-
-function integrator_expdecay_filter(gain::Real, RC::Real)
-    T = float(promote_type(typeof(gain), typeof(RC)))
-    α = 1 / (1 + RC)
-    Biquad(T(gain), T(-α), T(0), T(α - 1), T(0))
+    α = RC / (RC + 1)
+    # Biquad(T(α), T(-α), T(0), T(-α), T(0))
+    Biquad(T(α), T(-α), T(0), T(-α), T(0))
 end
 
 
@@ -42,9 +36,23 @@ function differentiator_filter(gain::Real)
 end
 
 
+function integrator_cr_filter(gain::Real, RC::Real)
+    T = float(promote_type(typeof(gain), typeof(RC)))
+    α = 1 / (1 + RC)
+    Biquad(T(gain), T(-α), T(0), T(α - 1), T(0))
+end
+
+
+function integrator_expdecay_filter(gain::Real, RC::Real)
+    T = float(promote_type(typeof(gain), typeof(RC)))
+    α = 1 / (1 + RC)
+    Biquad(T(gain), T(0), T(0), T(α - 1), T(0))
+end
+
+
 function simple_csa_response_filter(τ_rise::Real, τ_decay::Real, gain::Real = one(τ_rise))
     # TODO: Use a single biquad filter
 
     T = float(promote_type(promote_type(typeof(τ_rise), typeof(τ_decay)), typeof(gain)))
-    integrator_filter(T(τ_rise)) * rc_filter(T(τ_rise)) * expdecay_filter(T(τ_decay))
+    rc_filter(T(τ_rise)) * integrator_expdecay_filter(T(gain), T(τ_decay))
 end
